7964b78a8850bb31cc05ae617088dedb4e57e412
heWogithub63 <heworring@gmail.com>
2024-02-04 06:15:08 -0500

    next version

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ app.post('/MeinungsSpiegel', (req, res) =>{
 	const data = req.body;
 	res.status(200).json({"Message": "Data posted", data});
 	txts = data.toString().split(",");
-	console.log(txts);
 	requestPost().catch(console.error);
 })
 
